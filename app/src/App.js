4ce97ce7280bea48764c25e974e9b0e02795c44e
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-06-16 00:19:01 +0200

    wip

--- app/src/App.js
+++ app/src/App.js
@@ -16,6 +16,19 @@ const websocketProvider = new WebsocketProvider(
   ydoc
 );
 
+const stringToColour = function (str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  var colour = "#";
+  for (var i = 0; i < 3; i++) {
+    var value = (hash >> (i * 8)) & 0xff;
+    colour += ("00" + value.toString(16)).substr(-2);
+  }
+  return colour;
+};
+
 function App() {
   const monacoRef = useRef(null);
   function handleEditorDidMount(editor, monaco) {
@@ -37,6 +50,7 @@ function App() {
       console.log(websocketProvider);
       console.log("update", d);
     });
+    console.log(websocketProvider.awareness.clientID);
   }
 
   return (
